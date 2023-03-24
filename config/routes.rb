71bfc008aba3c5ba0d646c90c5e6895bb8d7eff0
Daniel Londoño <daniel.esloh@gmail.com>
2023-03-24 18:28:53 -0500

    creation and configuration of message, room and user models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :rooms do
+    resources :messages
+  end
   devise_for :users
   root 'pages#home'
   get 'pages/home'
+  get 'user/:id', to: 'users#show', as: 'user'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
